df47f5843fc1b2d8240ff3cdc280dcb257680af5
Stumpkin <banksjalen1@gmail.com>
2020-03-09 19:10:54 -0700

    Inital Start
    Starting the thing

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public WayPoint currentWayPoint;
+    public WaypointM waypointMangement;
+    public Vector3 currentPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/WaypointM.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointM : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int size = 3;
+    public List<WayPoint> waypoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
