7587d08abe7b83d71310faf286525d3b5932315c
Stumpkin <banksjalen0@gmail.com>
2020-03-23 18:32:20 -0700

    Them Changes
    RESTARTING

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,19 +4,33 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public WayPoint currentWayPoint;
-    public WaypointM waypointMangement;
-    public Vector3 currentPosition;
-    // Start is called before the first frame update
-    void Start()
+  public Waypoint currentDestination;
+  public WaypointManager waypointManager;
+  private int currentIndexWaypoint = 0;
+  public float speed = 1;
+
+  public void Initialize(WaypointManager waypointManager)
+  {
+    this.waypointManager = waypointManager;
+    GetNextWaypoint();
+    transform.position = currentDestination.transform.position; // Move to WP0
+    GetNextWaypoint();
+  }
+
+  void Update()
+  {
+    Vector3 direction = currentDestination.transform.position - transform.position;
+    if (direction.magnitude < .2f)
     {
-        currentPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.y);
+      GetNextWaypoint();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    transform.Translate(direction.normalized * speed * Time.deltaTime);
+  }
 
+  private void GetNextWaypoint()
+  {
+    currentDestination = waypointManager.GetNeWaypoint(currentIndexWaypoint);
+    currentIndexWaypoint++;
+  }
 }
--- Assets/Scripts/WayPoint.cs
+++ Assets/Scripts/WayPoint.cs
@@ -2,19 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WayPoint : MonoBehaviour
+public class Waypoint : MonoBehaviour
 {
-    private Vector3 location;
-    // Start is called before the first frame update
-    void Start()
-    {
-        location = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.y);
-
-    }
-
-    // Update is called once per frame
-    Vector3 getPosition()
-    {
-        return location;
-    }
+    
 }
